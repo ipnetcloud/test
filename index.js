f230e9b75d42c3da5aaf7d574812d1d4ba42ed8d
Javier Jaimes <111013586+javjj10@users.noreply.github.com>
2023-04-13 15:58:07 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,52 @@
+class Product {
+  constructor(title, description, price, thumbnail, code, stock) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.code = code;
+    this.stock = stock;
+  }
+}
+
+class ProductManager {
+  constructor() {
+    this.products = [];
+    this.currentId = 0;
+  }
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    // Validar que todos los campos sean obligatorios
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      console.error("Todos los campos son obligatorios");
+      return;
+    }
+
+    // Validar que el campo "code" no se repita
+    const existingProduct = this.products.find((product) => product.code === code);
+    if (existingProduct) {
+      console.error("El código ya existe");
+      return;
+    }
+
+    const product = new Product(title, description, price, thumbnail, code, stock);
+
+    // Asignar un id autoincrementable al producto
+    product.id = ++this.currentId;
+
+    this.products.push(product);
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const product = this.products.find((product) => product.id === id);
+    if (product) {
+      return product;
+    } else {
+      console.error("Not found");
+    }
+  }
+}
